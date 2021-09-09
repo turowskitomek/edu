e33f519d75e22380c73626f3fc079b557df58543
turowskitomek <89968953+turowskitomek@users.noreply.github.com>
2021-09-09 10:43:49 +0200

    Add files via upload

---
+++ zsl/operacje_na_zmiennych.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+//wersjaphp
+echo PHP_VERSION;
+
+$pow=2**10;
+echo '<br>'.$pow;//1024
+
+$x=10;
+$y=3;
+
+echo "<br>".$x<=>$y;
+
+/*
+komentarz
+wieloliniowy
+*/
+
+$x=1;
+$y=1.0;
+
+if($x==$y){
+	echo "Równe";
+}
+else{
+	echo "Różne";
+}
+
+if($x===$y){
+	echo "Równe";
+}
+else{
+	echo "Różne";
+}
+?>
+
+</body>
+</html>
